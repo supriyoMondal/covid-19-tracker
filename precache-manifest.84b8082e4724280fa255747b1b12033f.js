bf404c66903d21c3f949dd2e87b5ac18f7fbe4bc
Supriyo Mondal <hisupriyooo@gmail.com>
2020-04-20 21:30:41 +0530

    Updates

---
+++ precache-manifest.84b8082e4724280fa255747b1b12033f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a320a430da8f696bf65ac866c5d327c6",
+    "url": "//covid-19-tracker/index.html"
+  },
+  {
+    "revision": "6f3bfa1ba9518a25f69f",
+    "url": "//covid-19-tracker/static/css/main.ba8ba8f4.chunk.css"
+  },
+  {
+    "revision": "b2230bcc3e2a20e1ba75",
+    "url": "//covid-19-tracker/static/js/2.fc73d4b2.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "//covid-19-tracker/static/js/2.fc73d4b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f3bfa1ba9518a25f69f",
+    "url": "//covid-19-tracker/static/js/main.41ebf750.chunk.js"
+  },
+  {
+    "revision": "218072f9a2ffe8afba63",
+    "url": "//covid-19-tracker/static/js/runtime-main.1ada562c.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "//covid-19-tracker/static/media/image.d7265326.png"
+  }
+]);
